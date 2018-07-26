eecb6dbc1dcb7a3fcd21c9316d250c7fd451e523
suhen <1239604859@qq.com>
2018-07-26 23:35:42 +0800

    init ble start.

---
+++ lib.ble/src/main/java/com/suhen/android/libble/BLE.java
@@ -0,0 +1,12 @@
+package com.suhen.android.libble;
+
+/**
+ * Created by suhen
+ * 18-7-26.
+ * Email: 1239604859@qq.com
+ */
+public class BLE {
+    public static final String BT_MAC = "bt_mac_address";
+
+    public static final String BT_NO_PERMISSION = "Unable to get bluetooth permission";
+}
--- lib.ble/src/test/java/com/suhen/android/libble/ExampleUnitTest.java
+++ lib.ble/src/test/java/com/suhen/android/libble/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.suhen.android.libble;
 
+import com.suhen.android.libble.utils.ByteZipUtil;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +12,41 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private String mString = "/**\n" +
+            " * Example local unit test, which will execute on the development machine (host).\n" +
+            " *\n" +
+            " * @see <a href=\"http://d.android.com/tools/testing\">Testing documentation</a>\n" +
+            " */";
+
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void bzip2Test() {
+        byte[] origin = mString.getBytes();
+        System.out.println(origin.length);
+
+        byte[] bZip2 = ByteZipUtil.bZip2(origin);
+        System.out.println(bZip2.length);
+
+        //////
+        String str = new String(ByteZipUtil.unBZip2(bZip2));
+        System.out.println(str);
+    }
+
+    @Test
+    public void gzipTest() {
+        byte[] origin = mString.getBytes();
+        System.out.println(origin.length);
+
+        byte[] bZip2 = ByteZipUtil.gZip(origin);
+        System.out.println(bZip2.length);
+
+        //////
+        String str = new String(ByteZipUtil.unGZip(bZip2));
+        System.out.println(str);
     }
 }
